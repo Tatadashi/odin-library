eb495babaf18b258610f959fb8f612aab79fb434
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-02-01 01:23:10 -0800

    Add visual representation of library array
    HTML initial changed to have table and its headers. Add borders to table elements and remove margin. Add two functions, 1 to reset visual to initial and one to update based on array (reset before update whenever addBooktoLibrary() successfully)

--- script.js
+++ script.js
@@ -16,9 +16,36 @@ function addBookToLibrary(title, author, pages) {
       if (Number.isInteger(pages) && pages > 0) {
         const book = new Book(title, author, pages);
         myLibrary.push(book);
+        updateLibrary();
       }
     }
   } 
+}
+
+//update visually display of library array
+function updateLibrary() {
+  const table = document.querySelector(`table`);
+  resetLibary();
+  myLibrary.forEach(book => {
+    table.innerHTML +=
+    `<tr>
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+    </tr>`;
+  });
+}
+
+
+//remove all entries visually from table
+function resetLibary() {
+  const table = document.querySelector(`table`);
+  table.innerHTML =
+  `<tr>
+  <th>Title</th>
+  <th>Author</th>
+  <th>Number of Pages (Chapters if Web Novel)</th>
+  </tr>`;
 }
 
 addBookToLibrary(`Solo Leveling, Vol. 1`, `Chugong`, 322);
@@ -26,5 +53,3 @@ addBookToLibrary(`Everyone Else is a Returnee`, `Toika`, 378);
 addBookToLibrary(`Mushoku Tensei: Jobless Reincarnation, Vol. 1`, `Rifujin na Magonote`, 408);
 addBookToLibrary(`Lockwood & Co. The Screaming Staircase`, `Jonathan Stroud`, 440);
 addBookToLibrary(`League of Legends: Realms of Runeterra (Official Companion)`, `Riot Games`, 256);
-
-console.log(myLibrary);
